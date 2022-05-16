5f9ce9156bfc646dbf1fea2c8e0e49fe54cc9e8c
Nick Nellis <neni0701@gmail.com>
2022-05-16 10:14:58 -0500

    fixed script

--- workshops/gloo-mesh-demo/instruqt/install/install-gloo-mesh/gloo-mesh-dashboard.sh
+++ workshops/gloo-mesh-demo/instruqt/install/install-gloo-mesh/gloo-mesh-dashboard.sh
@@ -3,4 +3,4 @@
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # expose gloo mesh dashboard
-kubectl apply -f $SCRIPT_DIR/install-gloo-mesh/gateway.yaml
+kubectl apply -f $LOCAL_DIR/gateway.yaml
