3a0155075b6f1126ef8c984ccda339a750c19342
Nick Nellis <neni0701@gmail.com>
2022-05-16 16:14:10 -0500

    failover done

--- workshops/gloo-mesh-demo/instruqt/tracks/failover/skip.sh
+++ workshops/gloo-mesh-demo/instruqt/tracks/failover/skip.sh
@@ -3,6 +3,14 @@
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Step 1 - Enable Multi Cluster Frontend
-kubectl apply -n web-team --context $MGMT -f /workshop/tracks/failover/virtual-destination.yaml
+kubectl apply -n web-team --context $MGMT -f $LOCAL_DIR/virtual-destination.yaml
 
-kubectl apply -n web-team --context $MGMT -f /workshop/tracks/failover/route-table.yaml
+kubectl apply -n web-team --context $MGMT -f $LOCAL_DIR/route-table.yaml
+
+# Step 3 - Failover POlicy
+
+kubectl apply -n web-team --context $MGMT -f $LOCAL_DIR/failover-policy.yaml
+
+# Step 4 - Outlier Detection Policy 
+
+kubectl apply -n web-team --context $MGMT -f $LOCAL_DIR/outlier-detection-policy.yaml
--- workshops/gloo-mesh-demo/instruqt/tracks/multi-cluster/skip.sh
+++ workshops/gloo-mesh-demo/instruqt/tracks/multi-cluster/skip.sh
@@ -4,9 +4,9 @@ LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Step 1 - VirtualDestinations
 
-kubectl apply -n backend-apis-team --context $MGMT -f /workshop/tracks/multi-cluster/virtual-destinations.yaml --context $MGMT
+kubectl apply -n backend-apis-team --context $MGMT -f $LOCAL_DIR/virtual-destinations.yaml --context $MGMT
 
 # Step 2 - Update frontend
-kubectl apply -n web-ui --context $CLUSTER1 -f /workshop/tracks/multi-cluster/frontend-global-hosts.yaml
+kubectl apply -n web-ui --context $CLUSTER1 -f $LOCAL_DIR/frontend-global-hosts.yaml
 
 kubectl wait pod -n web-ui --context $CLUSTER1 --for=condition=Ready --all
