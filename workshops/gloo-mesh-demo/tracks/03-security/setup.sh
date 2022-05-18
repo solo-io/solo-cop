5f3514c0b9743c12ee434a0f94aef1b0e60f4669
Nick Nellis <neni0701@gmail.com>
2022-05-18 10:24:22 -0500

    updated access poliy

--- workshops/gloo-mesh-demo/tracks/03-security/setup.sh
+++ workshops/gloo-mesh-demo/tracks/03-security/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+kubectl apply --context $CLUSTER1 -f $LOCAL_DIR../../install/http-client/http-client.yaml
