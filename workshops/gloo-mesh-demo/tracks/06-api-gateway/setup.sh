355b3fc65aee0033ecae620662d477e8186939ed
Nick <nick.nellis@solo.io>
2022-06-05 13:53:33 -0500

    added rate limiting (#50)

--- workshops/gloo-mesh-demo/tracks/06-api-gateway/setup.sh
+++ workshops/gloo-mesh-demo/tracks/06-api-gateway/setup.sh
@@ -6,3 +6,6 @@ LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 $LOCAL_DIR/../../install/keycloak/setup.sh
 
 $LOCAL_DIR/../../install/gloo-mesh-addons/setup.sh
+
+# update the workspace settings to import the gloo-mesh-addons services
+kubectl apply -f $LOCAL_DIR/workspace-settings.yaml --context $MGMT
