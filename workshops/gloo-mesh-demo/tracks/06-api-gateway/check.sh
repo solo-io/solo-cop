fdbcb0c66f7e3075033dce52174669b75459db96
Nick Nellis <neni0701@gmail.com>
2022-06-06 07:55:58 -0500

    fixed message

--- workshops/gloo-mesh-demo/tracks/06-api-gateway/check.sh
+++ workshops/gloo-mesh-demo/tracks/06-api-gateway/check.sh
@@ -4,4 +4,4 @@ set -e
 
 kubectl get WAFPolicy log4jshell -n web-team --context $MGMT || fail-message "Could not find the failover WAFPolicy in the web-team namespace in the mgmt cluster"
 kubectl get ExtAuthPolicy frontend -n web-team --context $MGMT || fail-message "Could not find the frontend ExtAuthPolicy in the web-team namespace in the mgmt cluster"
-kubectl get RateLimitPolicy rate-limit-policy -n web-team --context $MGMT || fail-message "Could not find the frontend RateLimitPolicy in the web-team namespace in the mgmt cluster"
+kubectl get RateLimitPolicy rate-limit-policy -n web-team --context $MGMT || fail-message "Could not find the rate-limit-policy RateLimitPolicy in the web-team namespace in the mgmt cluster"
