5d70539f3227cba42541f72fce6a99d621a764b6
Nick Nellis <nick.nellis@solo.io>
2022-06-05 19:39:03 -0500

    remove load generator

--- workshops/gloo-mesh-demo/tracks/06-api-gateway/setup.sh
+++ workshops/gloo-mesh-demo/tracks/06-api-gateway/setup.sh
@@ -9,3 +9,6 @@ $LOCAL_DIR/../../install/gloo-mesh-addons/setup.sh
 
 # update the workspace settings to import the gloo-mesh-addons services
 kubectl apply -f $LOCAL_DIR/workspace-settings.yaml --context $MGMT
+
+# remove the load generator so we dont disturb rate limiting
+kubectl delete deployment loadgenerator -n web-ui --context $CLUSTER1
