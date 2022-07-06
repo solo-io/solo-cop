e423772ddf8fe1dd78587b0c508dbe7fa9aa59e6
Nick Nellis <neni0701@gmail.com>
2022-07-05 19:56:34 -0500

    wait for service

--- workshops/gloo-gateway/install/keycloak/setup.sh
+++ workshops/gloo-gateway/install/keycloak/setup.sh
@@ -7,15 +7,15 @@ kubectl create namespace keycloak
 kubectl apply -n keycloak -f $LOCAL_DIR/keycloak.yaml
 kubectl wait deployment --for condition=Available=True -n keycloak --timeout 60s --all --timeout 180s
 
+# wait for service load balancer
+until kubectl get service/keycloak -n keycloak --output=jsonpath='{.status.loadBalancer}' | grep "ingress"; do : ; done
+
 POD_NAME=$(kubectl get pods -n keycloak -l app=keycloak -o json | jq -r '.items[0].metadata.name')
 
 kubectl apply -f $LOCAL_DIR/keycloak-setup-pod.yaml
 kubectl wait --for=condition=ready pod -l app=keycloak-setup -n keycloak --timeout 60s
 
 kubectl -n keycloak cp $LOCAL_DIR/gen-clientid.sh keycloak-setup:/tmp/gen-clientid.sh
-
-# wait for service load balancer
-until kubectl get service/keycloak -n keycloak --output=jsonpath='{.status.loadBalancer}' | grep "ingress"; do : ; done
 
 echo "Keycloak authorized endpoint: $GLOO_GATEWAY"
 kubectl exec -n keycloak -it keycloak-setup -- env KEYCLOAK_URL=$KEYCLOAK_URL /tmp/gen-clientid.sh $GLOO_GATEWAY
