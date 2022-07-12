379cde5fd01daf18fb5a1dfd4e150ac23fdba89b
Nick Nellis <neni0701@gmail.com>
2022-07-12 10:54:18 -0500

    remove change

--- workshops/gloo-mesh-demo/install/keycloak/setup.sh
+++ workshops/gloo-mesh-demo/install/keycloak/setup.sh
@@ -7,9 +7,6 @@ kubectl create namespace keycloak --context $CLUSTER1
 kubectl apply --context $CLUSTER1 -n keycloak -f $LOCAL_DIR/keycloak.yaml
 kubectl wait deployment --for condition=Available=True -n keycloak --timeout 60s --context $CLUSTER1 --all --timeout 180s
 
-# wait for service load balancer
-until kubectl get service/keycloak -n keycloak --output=jsonpath='{.status.loadBalancer}' | grep "ingress"; do : ; done
-
 POD_NAME=$(kubectl get pods -n keycloak -l app=keycloak --context $CLUSTER1 -o json | jq -r '.items[0].metadata.name')
 
 # not wait for service load balancer
