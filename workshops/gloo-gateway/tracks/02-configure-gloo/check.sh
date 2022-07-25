97f7b5253ede94b7dd8b8a62c4042fbe71e36c6f
Nick Nellis <nick.nellis@solo.io>
2022-07-24 20:17:38 -0500

    scripts for first and second labs

---
+++ workshops/gloo-gateway/tracks/02-configure-gloo/check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+kubectl get deployment gloo-mesh-mgmt-server -n gloo-mesh || fail-message "Could not find the gloo-mesh-mgmt-server in the gloo-mesh namespace, was Gloo platform installed?"
+
+kubectl get deployment gloo-gateway -n gloo-gateway || fail-message "Could not find the gloo-gateway deployment in the gloo-gateway namespace, was Gloo gateway installed?"
+
+kubectl get namespace gloo-gateway-addons || fail-message "Could not find the gloo-gateway-addons namespace, was the Gloo gateway addons installed?"
+
+kubectl get ExtAuthServer ext-auth-server -n dev-team || fail-message "Cloud not find the ExtAuthServer named ext-auth-server in the dev-team namespace, was Gloo gateway addons installed?"
