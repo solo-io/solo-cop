9cb6d81bc6149378e1e149ac73ca396bd5d85c77
Nick <nick.nellis@solo.io>
2022-10-17 15:19:10 -0500

    addec context (#85)

--- workshops/gloo-mesh-demo/tracks/06-api-gateway/setup.sh
+++ workshops/gloo-mesh-demo/tracks/06-api-gateway/setup.sh
@@ -10,4 +10,4 @@ $LOCAL_DIR/../../install/gloo-mesh-addons/setup.sh
 # remove the load generator so we dont disturb rate limiting
 kubectl delete deployment loadgenerator -n web-ui --context $CLUSTER1
 
-kubectl apply -f $LOCAL_DIR/gloo-mesh-addons-servers.yaml
+kubectl apply -f $LOCAL_DIR/gloo-mesh-addons-servers.yaml --context $MGMT
