1f7ac82e29a3067020ae5f7754c9748729996840
Nick <nick.nellis@solo.io>
2022-10-31 13:11:25 -0500

    Remove federation (#91)
    * working istio 1.15 with tc3
    
    * working istio 1.15 with tc3
    
    * Update workshops/gloo-mesh-demo/README.md
    
    Co-authored-by: Ram Vennam <ram.vennam@solo.io>
    
    Co-authored-by: Ram Vennam <ram.vennam@solo.io>

--- workshops/gloo-mesh-demo/tracks/02-workspaces/setup.sh
+++ workshops/gloo-mesh-demo/tracks/02-workspaces/setup.sh
@@ -4,7 +4,7 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
    -keyout tls.key -out tls.crt -subj "/CN=*"
 
-kubectl --context ${CLUSTER1} -n istio-gateways create secret generic tls-secret \
+kubectl --context ${CLUSTER1} -n istio-ingress create secret generic tls-secret \
 --from-file=tls.key=tls.key \
 --from-file=tls.crt=tls.crt
 
